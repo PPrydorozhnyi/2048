97cc15cfe0ddabb7d17fe9a5d5b684a6a38225ff
Peter Pridorozhny <Peter35128@gmail.com>
2017-08-07 05:37:15 +0300

    Changed multiple if to switch statement

--- src/game/Tile.java
+++ src/game/Tile.java
@@ -34,42 +34,56 @@ public class Tile {
     private void drawImage() {
 
         Graphics2D g = (Graphics2D)tileImage.getGraphics();
-        if (value == 2) {
-            backgroundColor = new Color(0xe9e9e9);
-            textColor = new Color(0, 0, 0);
-        } else if (value == 4) {
-            backgroundColor = new Color(0xe6daab);
-            textColor = new Color(0,0,0);
-        } else if (value == 8) {
-            backgroundColor = new Color(0xf70d3d);
-            textColor = new Color(0xffffff);
-        } else if (value == 16) {
-            backgroundColor = new Color(0xf28007);
-            textColor = new Color(0xffffff);
-        } else if (value == 32) {
-            backgroundColor = new Color(0xf55e3b);
-            textColor = new Color(0xffffff);
-        } else if (value == 64) {
-            backgroundColor = new Color(0xff0000);
-            textColor = new Color(0xffffff);
-        } else if (value == 128) {
-            backgroundColor = new Color(0xe9de84);
-            textColor = new Color(0xffffff);
-        } else if (value == 256) {
-            backgroundColor = new Color(0xf6e873);
-            textColor = new Color(0xffffff);
-        } else if (value == 512) {
-            backgroundColor = new Color(0xf5e455);
-            textColor = new Color(0xffffff);
-        } else if (value == 1024) {
-            backgroundColor = new Color(0xf7e12c);
-            textColor = new Color(0xffffff);
-        } else if (value == 2048) {
-            backgroundColor = new Color(0xffe400);
-            textColor = new Color(0xffffff);
-        } else {
-            backgroundColor = Color.BLACK;
-            textColor = Color.WHITE;
+
+        switch(value) {
+            case 2:
+                backgroundColor = new Color(0xe9e9e9);
+                textColor = new Color(0, 0, 0);
+                break;
+            case 4:
+                backgroundColor = new Color(0xe6daab);
+                textColor = new Color(0,0,0);
+                break;
+            case 8:
+                backgroundColor = new Color(0xf70d3d);
+                textColor = new Color(0xffffff);
+                break;
+            case 16:
+                backgroundColor = new Color(0xf28007);
+                textColor = new Color(0xffffff);
+                break;
+            case 32:
+                backgroundColor = new Color(0xf55e3b);
+                textColor = new Color(0xffffff);
+                break;
+            case 64:
+                backgroundColor = new Color(0xff0000);
+                textColor = new Color(0xffffff);
+                break;
+            case 128:
+                backgroundColor = new Color(0xe9de84);
+                textColor = new Color(0xffffff);
+                break;
+            case 256:
+                backgroundColor = new Color(0xf6e873);
+                textColor = new Color(0xffffff);
+                break;
+            case 512:
+                backgroundColor = new Color(0xf5e455);
+                textColor = new Color(0xffffff);
+                break;
+            case 1024:
+                backgroundColor = new Color(0xf7e12c);
+                textColor = new Color(0xffffff);
+                break;
+            case 2048:
+                backgroundColor = new Color(0xffe400);
+                textColor = new Color(0xffffff);
+                break;
+            default:
+                backgroundColor = Color.BLACK;
+                textColor = Color.WHITE;
+                break;
         }
 
         g.setColor(new Color(0,0,0));
