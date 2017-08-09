7ae34d7636f3a08d3ee7cf9dae662b605fea16b9
Peter Pridorozhny <Peter35128@gmail.com>
2017-08-09 18:06:17 +0300

    Buttons class added

--- src/gui/GuiPanel.java
+++ src/gui/GuiPanel.java
@@ -2,34 +2,55 @@ package gui;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 /**
  * Created by drake on 08/08/17.
  */
 public class GuiPanel {
 
-    public void update() {
+    private ArrayList<GuiButton> buttons;
 
+    public GuiPanel() {
+        buttons = new ArrayList<GuiButton>();
+    }
+
+    public void update() {
+        for (GuiButton b : buttons)
+            b.update();
     }
 
     public void render(Graphics2D g) {
+        for (GuiButton b : buttons)
+            b.render(g);
+    }
 
+    public void add(GuiButton button) {
+        buttons.add(button);
+    }
+
+    public void remove(GuiButton button) {
+        buttons.remove(button)
     }
 
     public void mousePressed(MouseEvent e) {
-
+        for (GuiButton b : buttons)
+            b.mousePressed(e);
     }
 
     public void mouseReleased(MouseEvent e) {
-
+        for (GuiButton b : buttons)
+            b.mouseReleased(e);
     }
 
     public void mouseDragged(MouseEvent e) {
-
+        for (GuiButton b : buttons)
+            b.mouseDragged(e);
     }
 
     public void mouseMoved(MouseEvent e) {
-
+        for (GuiButton b : buttons)
+            b.mouseMoved(e);
     }
 
 }
---
+++ src/gui/State.java
@@ -0,0 +1,10 @@
+package gui;
+
+/**
+ * Created by drake on 09/08/17.
+ */
+public enum State {
+    RELEASED,
+    HOVER,
+    PRESSED
+}
